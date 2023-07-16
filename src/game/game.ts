32ff1e8b81be79631ea659c41878fea707a65812
Johnny Reina <johnny.r.reina@gmail.com>
2023-07-15 19:34:30 -0500

    refactor: separate game and bot

--- src/game/game.ts
+++ src/game/game.ts
@@ -3,6 +3,9 @@ import chance from "chance";
 import EventEmitter from "events";
 import { AnyRecord } from "dns";
 import { Func } from "../types/Func";
+import { parseExpression } from "../utils/parseExpression";
+
+const CARDS_PER_ROUND = 4;
 
 export type Round = [Card, Card, Card, Card];
 
@@ -18,7 +21,16 @@ export enum GameEvent {
   DiscardsChanged = "onDiscardsChanged",
   CurrentChanged = "onCurrentChanged",
   GameFinished = "onGameFinished",
+  PointScored = "onPointScored",
+  AttemptIncorrect = "onAttemptIncorrect",
+  AttemptInvalid = "onAttemptInvalid",
+  RoundSkipped = "onRoundSkipped",
 }
+
+type GameConfig = {
+  includeFaceCards: boolean;
+  logger?: (message: string) => void;
+};
 
 export class Game {
   private _deck!: Array<Card>;
@@ -29,16 +41,23 @@ export class Game {
 
   private _gameEventEmitter = new EventEmitter();
 
-  constructor(private _includeFaceCards = false) {
-    this._initDeck();
+  constructor(private _gameConfig: GameConfig) {
   }
 
-  public draw(): void {
+  public start(): void {
+    this._initDeck();
+    this._draw();
+  }
+
+  private _draw(): void {
     if (this._deck.length === 0) {
+      this._log("Draw: No cards left in deck");
       this._gameEventEmitter.emit(GameEvent.GameFinished);
       return;
     }
-    this._current = this._chance.pickset(this._deck, 4) as Round;
+
+    this._log("Drawing new round");
+    this._current = this._chance.pickset(this._deck, CARDS_PER_ROUND) as Round;
     this._deck = this._deck.filter((item) => !this._current.includes(item));
     this._discards.push(...this._current);
     this._gameEventEmitter.emit(GameEvent.CurrentChanged, this._current);
@@ -46,45 +65,76 @@ export class Game {
     this._gameEventEmitter.emit(GameEvent.DiscardsChanged, this._discards);
   }
 
-  public get current() {
+  public get current(): Round {
     return this._current;
   }
 
-  public get discards() {
+  public get discards(): Array<Card> {
     return this._discards;
   }
 
-  public get deck() {
+  public get deck(): Array<Card> {
     return this._deck;
   }
 
-  public get players() {
+  public get players(): Array<PlayerData> {
     return this._players;
   }
 
-  public addPoint(playerId: string) {
+  public attemptSolution(playerId: string, solution: string): void {
+    this._log(`Player ${playerId} attempted solution ${solution}`);
+    const result = parseExpression(solution, this._current);
+
+    this._log(`Result: ${JSON.stringify(result)}`);
+
+    if (!result.valid) {
+      this._gameEventEmitter.emit(GameEvent.AttemptInvalid, {
+        playerId,
+        expr: solution,
+      });
+    } else if (result.value !== 24) {
+      this._gameEventEmitter.emit(GameEvent.AttemptIncorrect, {
+        playerId,
+        expr: solution,
+        value: result.value,
+      });
+    } else {
+      this._addPoint(playerId);
+      this._draw();
+    }
+  }
+
+  public skipRound(playerId: string): void {
+    this._log(`Player ${playerId} skipped round`);
+    this._draw();
+    this._gameEventEmitter.emit(GameEvent.RoundSkipped, this._current);
+  }
+
+  private _addPoint(playerId: string): void {
+    this._log(`Player ${playerId} scored a point`);
+
     const player =
       this._players.find((player) => player.id === playerId) ||
-      this.addPlayer(playerId);
+      this._addPlayer(playerId);
 
     player.score += 1;
     player.cards.push(...this._current);
+    this._gameEventEmitter.emit(GameEvent.PointScored, player);
   }
 
   /**
    * Adds an empty player with the specified id.
    */
-  public addPlayer(id: string) {
+  private _addPlayer(id: string) {
+    this._log(`Adding player ${id}`);
+
     if (this._players.find((player) => player.id === id)) {
-      throw new Error(`Player with id ${id} already exists`);
+      this._log(`Player with id ${id} already exists`);
     }
     const player = { id, cards: [], score: 0 };
     this._players.push(player);
+    this._gameEventEmitter.emit(GameEvent.PlayersChanged, this._players);
     return player;
-  }
-
-  public get gameEvents() {
-    return this._gameEventEmitter;
   }
 
   public addListener(
@@ -100,6 +150,22 @@ export class Game {
     handler: Func<Array<PlayerData>, void>
   ): void;
   public addListener(event: GameEvent.GameFinished, handler: () => void): void;
+  public addListener(
+    event: GameEvent.PointScored,
+    handler: Func<PlayerData, void>
+  ): void;
+  public addListener(
+    event: GameEvent.RoundSkipped,
+    handler: Func<Round, void>
+  ): void;
+  public addListener(
+    event: GameEvent.AttemptIncorrect,
+    handler: Func<{ playerId: string; expr: string; value: number }, void>
+  ): void;
+  public addListener(
+    event: GameEvent.AttemptInvalid,
+    handler: Func<{ playerId: string; expr: string }, void>
+  ): void;
   public addListener(event: GameEvent, handler: Func<any, void>): void {
     this._gameEventEmitter.on(event, handler);
   }
@@ -108,19 +174,25 @@ export class Game {
     this._gameEventEmitter.off(event, handler);
   }
 
-  public restart() {
+  public restart(): void {
+    this._log("Restarting game");
+
     this._players.forEach((player) => {
       player.score = 0;
       player.cards = [];
     });
     this._initDeck();
-    this.draw();
+    this._draw();
     this._gameEventEmitter.emit(GameEvent.PlayersChanged, this._players);
   }
 
-  private _initDeck() {
-    this._deck = this._includeFaceCards
+  private _initDeck(): void {
+    this._deck = this._gameConfig.includeFaceCards
       ? [...Deck]
       : [...Deck.filter((card) => card.number < 11)];
+  }
+
+  private _log(message: string) {
+    this._gameConfig.logger?.(message);
   }
 }
---
+++ src/solver/solver.ts
@@ -0,0 +1,48 @@
+import { Card } from "../game/deck";
+
+/**
+ * Copilot suggested this function to solve the 24 game.
+ * It also suggested this comment.
+ */
+function solveHelper(
+  numbers: number[],
+  expr: string,
+  results: Array<string>
+): void {
+  if (numbers.length === 1) {
+    if (numbers[0] === 24) {
+      results.push(expr);
+    }
+    return;
+  }
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (i !== j) {
+        const a = numbers[i];
+        const b = numbers[j];
+        const remaining = numbers.filter((_, k) => k !== i && k !== j);
+        solveHelper([...remaining, a + b], `(${expr} ${a}+${b})`, results);
+        solveHelper([...remaining, a - b], `(${expr} ${a}-${b})`, results);
+        solveHelper([...remaining, b - a], `(${expr} ${b}-${a})`, results);
+        solveHelper([...remaining, a * b], `(${expr} ${a}*${b})`, results);
+        if (b !== 0) {
+          solveHelper([...remaining, a / b], `(${expr} ${a}/${b})`, results);
+        }
+        if (a !== 0) {
+          solveHelper([...remaining, b / a], `(${expr} ${b}/${a})`, results);
+        }
+      }
+    }
+  }
+}
+
+export function solve(cards: Card[]): string[] {
+  const results: string[] = [];
+
+  solveHelper(
+    cards.map((card) => card.number),
+    "",
+    results
+  );
+  return results;
+}
